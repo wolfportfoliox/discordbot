425ab74776ca3b554c86d332092482eaa742eeee
WOLF <wolfportfoliox@gmail.com>
2026-05-10 11:49:16 +0500

    Fix CLIENT_ID to DISCORD_CLIENT_ID

--- deploy-commands.js
+++ deploy-commands.js
@@ -26,12 +26,12 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
     console.log('Registering commands...');
 
     await rest.put(
-  Routes.applicationGuildCommands(
-    process.env.CLIENT_ID,
-    process.env.GUILD_ID
-  ),
-  { body: commands }
-);
+    Routes.applicationGuildCommands(
+      process.env.DISCORD_CLIENT_ID,
+      process.env.GUILD_ID
+    ),
+    { body: commands }
+  );
 
     console.log('Commands registered successfully');
   } catch (error) {
