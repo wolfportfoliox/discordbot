c4c68de5cfd28493f827b093d5ef2f48db8b250d
WOLF <wolfportfoliox@gmail.com>
2026-05-10 12:19:54 +0500

    Add topPerformerScheduler, use clientReady event

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,16 @@
-import { marketScheduler, newsScheduler, checkPriceAlerts } from '../utils/schedulers.js';
+import { marketScheduler, newsScheduler, checkPriceAlerts, topPerformerScheduler } from '../utils/schedulers.js';
 
 export default {
-  name: 'ready',
+  name: 'clientReady',
   once: true,
   execute(client) {
-    console.log(`✅ Bot logged in as ${client.user.tag}`);
-    console.log(`📡 MARKET_CHANNEL_ID: ${process.env.MARKET_CHANNEL_ID}`);
-    console.log(`📡 NEWS_CHANNEL_ID: ${process.env.NEWS_CHANNEL_ID}`);
+    console.log(`Bot logged in as ${client.user.tag}`);
+    console.log(`MARKET_CHANNEL_ID: ${process.env.MARKET_CHANNEL_ID}`);
+    console.log(`NEWS_CHANNEL_ID: ${process.env.NEWS_CHANNEL_ID}`);
 
     marketScheduler(client);
     newsScheduler(client);
     checkPriceAlerts(client);
+    topPerformerScheduler(client);
   },
 };
